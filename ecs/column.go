952b962de7b5bc60874b1e20be725cb1aa232037
mlange-42 <martin_lange_@gmx.net>
2025-02-19 15:54:40 +0100

    add more column functionality

--- ecs/column.go
+++ ecs/column.go
@@ -10,6 +10,7 @@ type column struct {
 	data     reflect.Value
 	pointer  unsafe.Pointer
 	itemSize uintptr
+	len      uint32
 }
 
 func newColumn(tp reflect.Type, capacity int) column {
@@ -23,11 +24,26 @@ func newColumn(tp reflect.Type, capacity int) column {
 		data:     data,
 		pointer:  pointer,
 		itemSize: size,
+		len:      0,
 	}
+}
+
+func (c *column) Len() int {
+	return int(c.len)
+}
+
+func (c *column) Cap() int {
+	return c.data.Cap()
 }
 
 func (c *column) Get(index uint32) unsafe.Pointer {
 	return unsafe.Pointer(uintptr(c.pointer) + uintptr(index)*c.itemSize)
+}
+
+func (c *column) Add(comp unsafe.Pointer) unsafe.Pointer {
+	c.Extend(1)
+	c.len++
+	return c.Set(c.len-1, comp)
 }
 
 func (c *column) Set(index uint32, comp unsafe.Pointer) unsafe.Pointer {
@@ -40,12 +56,35 @@ func (c *column) Set(index uint32, comp unsafe.Pointer) unsafe.Pointer {
 	return dst
 }
 
-func (c *column) Extend(capacity int) {
+func (c *column) Remove(index uint32) bool {
+	lastIndex := uint32(c.len - 1)
+	swapped := index != lastIndex
+
+	if swapped {
+		src := unsafe.Add(c.pointer, lastIndex*uint32(c.itemSize))
+		dst := unsafe.Add(c.pointer, index*uint32(c.itemSize))
+		copyPtr(src, dst, c.itemSize)
+	}
+	c.len--
+	// TODO: zero the last element?
+	return swapped
+}
+
+func (c *column) Extend(by int) {
+	required := c.Len() + by
+	cap := c.Cap()
+	if cap >= required {
+		return
+	}
+	for cap < required {
+		cap *= 2
+	}
+
 	if c.itemSize == 0 {
 		return
 	}
 	old := c.data
-	c.data = reflect.New(reflect.ArrayOf(capacity, old.Type().Elem())).Elem()
+	c.data = reflect.New(reflect.ArrayOf(cap, old.Type().Elem())).Elem()
 	c.pointer = c.data.Addr().UnsafePointer()
 	reflect.Copy(c.data, old)
 }
--- ecs/column_test.go
+++ ecs/column_test.go
@@ -3,6 +3,7 @@ package ecs
 import (
 	"reflect"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -12,4 +13,33 @@ func TestColumnPointer(t *testing.T) {
 	column := newColumn(posType, 8)
 
 	assert.Equal(t, uintptr(column.pointer), uintptr(column.data.Addr().UnsafePointer()))
+}
+
+func TestColumnAddRemove(t *testing.T) {
+	posType := reflect.TypeOf(Position{})
+	column := newColumn(posType, 8)
+
+	assert.Equal(t, 8, column.Cap())
+	assert.Equal(t, 0, column.Len())
+
+	column.Add(unsafe.Pointer(&Position{1, 2}))
+	column.Add(unsafe.Pointer(&Position{3, 4}))
+	column.Add(unsafe.Pointer(&Position{5, 6}))
+
+	assert.Equal(t, 8, column.Cap())
+	assert.Equal(t, 3, column.Len())
+
+	pos := (*Position)(column.Get(2))
+	assert.Equal(t, Position{5, 6}, *pos)
+
+	swapped := column.Remove(0)
+	assert.True(t, swapped)
+	assert.Equal(t, 2, column.Len())
+
+	pos = (*Position)(column.Get(0))
+	assert.Equal(t, Position{5, 6}, *pos)
+
+	swapped = column.Remove(1)
+	assert.False(t, swapped)
+	assert.Equal(t, 1, column.Len())
 }
