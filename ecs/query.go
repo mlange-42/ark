5f95ce0b0dd250ab821ec38d206b138d4598829a
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-02-20 20:00:28 +0100

    README, query cursor (#9)

--- ecs/query.go
+++ ecs/query.go
@@ -1,17 +1,20 @@
 package ecs
 
+type cursor struct {
+	table    int
+	index    uintptr
+	maxIndex int64
+}
+
 // Query2 is a filter for two components.
 type Query2[A any, B any] struct {
 	world      *World
 	mask       Mask
+	cursor     cursor
 	componentA *componentStorage
 	componentB *componentStorage
 	columnA    *column
 	columnB    *column
-
-	table    int
-	index    uintptr
-	maxIndex int64
 }
 
 // NewQuery2 creates a new [Query2].
@@ -24,16 +27,17 @@ func NewQuery2[A any, B any](world *World) Query2[A, B] {
 		mask:       All(idA, idB),
 		componentA: &world.storage.components[idA.id],
 		componentB: &world.storage.components[idB.id],
-
-		table:    -1,
-		index:    0,
-		maxIndex: -1,
+		cursor: cursor{
+			table:    -1,
+			index:    0,
+			maxIndex: -1,
+		},
 	}
 }
 
 func (q *Query2[A, B]) Next() bool {
-	if int64(q.index) < q.maxIndex {
-		q.index++
+	if int64(q.cursor.index) < q.cursor.maxIndex {
+		q.cursor.index++
 		return true
 	}
 	return q.nextTable()
@@ -41,27 +45,27 @@ func (q *Query2[A, B]) Next() bool {
 
 func (q *Query2[A, B]) nextTable() bool {
 	maxTableIndex := len(q.world.storage.tables) - 1
-	for q.table < maxTableIndex {
-		q.table++
-		table := &q.world.storage.tables[q.table]
+	for q.cursor.table < maxTableIndex {
+		q.cursor.table++
+		table := &q.world.storage.tables[q.cursor.table]
 		archetype := &q.world.storage.archetypes[table.archetype]
 		if !archetype.mask.Contains(&q.mask) || table.entities.Len() == 0 {
 			continue
 		}
 
-		q.columnA = q.componentA.columns[q.table]
-		q.columnB = q.componentB.columns[q.table]
-		q.index = 0
-		q.maxIndex = int64(table.entities.Len() - 1)
+		q.columnA = q.componentA.columns[q.cursor.table]
+		q.columnB = q.componentB.columns[q.cursor.table]
+		q.cursor.index = 0
+		q.cursor.maxIndex = int64(table.entities.Len() - 1)
 		return true
 	}
-	q.table = -1
-	q.index = 0
-	q.maxIndex = -1
+	q.cursor.table = -1
+	q.cursor.index = 0
+	q.cursor.maxIndex = -1
 	return false
 }
 
 func (q *Query2[A, B]) Get() (*A, *B) {
-	return (*A)(q.columnA.Get(q.index)),
-		(*B)(q.columnB.Get(q.index))
+	return (*A)(q.columnA.Get(q.cursor.index)),
+		(*B)(q.columnB.Get(q.cursor.index))
 }
