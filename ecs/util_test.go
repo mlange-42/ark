89fe087ec6fc52ab44a2f6357a5e4bdd9a8cb6d3
mlange-42 <martin_lange_@gmx.net>
2025-02-21 14:32:48 +0100

    add benchmarks for registry and type operations

--- ecs/util_test.go
+++ ecs/util_test.go
@@ -10,3 +10,16 @@ func TestTypeOf(t *testing.T) {
 	posType := typeOf[Position]()
 	assert.Equal(t, "Position", posType.Name())
 }
+
+func BenchmarkTypeOf(b *testing.B) {
+	for b.Loop() {
+		_ = typeOf[Position]()
+	}
+}
+
+func BenchmarkSizeOf(b *testing.B) {
+	tp := typeOf[Position]()
+	for b.Loop() {
+		_ = sizeOf(tp)
+	}
+}
