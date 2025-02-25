9394a89fc26d6e4afcfabd82b274ccd555122664
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-02-25 18:02:22 +0100

    Add an example to the README (#74)

---
+++ _examples/readme/main.go
@@ -0,0 +1,55 @@
+// The minimal example from the README,
+// for automatic testing by the GitHub CI.
+package main
+
+import (
+	"math/rand/v2"
+
+	"github.com/mlange-42/ark/ecs"
+)
+
+// Position component
+type Position struct {
+	X float64
+	Y float64
+}
+
+// Velocity component
+type Velocity struct {
+	X float64
+	Y float64
+}
+
+func main() {
+	// Create a World with given initial capacity.
+	world := ecs.NewWorld(1024)
+
+	// Create a component mapper.
+	mapper := ecs.NewMap2[Position, Velocity](&world)
+
+	// Create entities.
+	for i := 0; i < 1000; i++ {
+		// Create a new Entity with components.
+		_ = mapper.NewEntity(
+			&Position{X: rand.Float64() * 100, Y: rand.Float64() * 100},
+			&Velocity{X: rand.NormFloat64(), Y: rand.NormFloat64()},
+		)
+	}
+
+	// Create a generic filter.
+	filter := ecs.NewFilter2[Position, Velocity](&world)
+
+	// Time loop.
+	for t := 0; t < 1000; t++ {
+		// Get a fresh query.
+		query := filter.Query()
+		// Iterate it
+		for query.Next() {
+			// Component access through the Query.
+			pos, vel := query.Get()
+			// Update component fields.
+			pos.X += vel.X
+			pos.Y += vel.Y
+		}
+	}
+}
