0ea072138e9193f1338e5b6559ee0fcc7849b78f
mlange-42 <martin_lange_@gmx.net>
2025-02-25 16:05:42 +0100

    Add test for component registry

--- ecs/registry_test.go
+++ ecs/registry_test.go
@@ -1,6 +1,48 @@
 package ecs
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestComponentRegistry(t *testing.T) {
+	reg := newComponentRegistry()
+
+	posType := reflect.TypeOf((*Position)(nil)).Elem()
+	rotType := reflect.TypeOf((*Velocity)(nil)).Elem()
+
+	reg.registerComponent(posType, MaskTotalBits)
+	assert.Equal(t, []uint8{uint8(0)}, reg.IDs)
+
+	reg.registerComponent(rotType, MaskTotalBits)
+	reg.unregisterLastComponent()
+	assert.Equal(t, []uint8{uint8(0)}, reg.IDs)
+
+	id0, _ := reg.ComponentID(posType)
+	id1, _ := reg.ComponentID(rotType)
+	assert.Equal(t, uint8(0), id0)
+	assert.Equal(t, uint8(1), id1)
+
+	assert.Equal(t, []uint8{uint8(0), uint8(1)}, reg.IDs)
+
+	t1, _ := reg.ComponentType(uint8(0))
+	t2, _ := reg.ComponentType(uint8(1))
+
+	assert.Equal(t, posType, t1)
+	assert.Equal(t, rotType, t2)
+}
+
+func TestComponentRegistryOverflow(t *testing.T) {
+	reg := newComponentRegistry()
+
+	reg.registerComponent(reflect.TypeOf((*Position)(nil)).Elem(), 1)
+
+	assert.PanicsWithValue(t, "exceeded the maximum of 1 component types or resource types", func() {
+		reg.registerComponent(reflect.TypeOf((*Velocity)(nil)).Elem(), 1)
+	})
+}
 
 func BenchmarkRegistryGet(b *testing.B) {
 	w := NewWorld(1024)
