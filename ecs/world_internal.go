5310b09e93b4d86e365a7cce83dd74766dee822a
mlange-42 <martin_lange_@gmx.net>
2025-03-02 17:12:08 +0100

    implement table exchange

--- ecs/world_internal.go
+++ ecs/world_internal.go
@@ -107,6 +107,112 @@ func (w *World) exchange(entity Entity, add []ID, rem []ID, addComps []unsafe.Po
 	w.storage.registerTargets(relations)
 }
 
+func (w *World) exchangeBatch(batch *Batch, add []ID, rem []ID, addComps []unsafe.Pointer, relations []RelationID) int {
+	w.checkLocked()
+
+	if len(add) == 0 && len(rem) == 0 {
+		if len(relations) > 0 {
+			panic("exchange operation has no effect, but a relation is specified. Use Batch.SetRelation instead")
+		}
+		return 0
+	}
+
+	tables := w.getTables(batch)
+	lengths := make([]uint32, len(tables))
+	var totalEntities uint32 = 0
+	for i, table := range tables {
+		lengths[i] = uint32(table.Len())
+		totalEntities += uint32(table.Len())
+	}
+
+	for i, table := range tables {
+		tableLen := lengths[i]
+
+		if tableLen == 0 {
+			continue
+		}
+		_, _ = w.exchangeTable(table, int(tableLen), add, rem, addComps, relations)
+	}
+
+	return int(totalEntities)
+}
+
+func (w *World) exchangeTable(oldTable *table, oldLen int, add []ID, rem []ID, addComps []unsafe.Pointer, relations []RelationID) (*table, uintptr) {
+	w.checkLocked()
+
+	oldArchetype := &w.storage.archetypes[oldTable.archetype]
+
+	mask := oldArchetype.mask
+	w.storage.getExchangeMask(&mask, add, rem)
+
+	oldIDs := oldArchetype.components
+
+	newTable := w.storage.findOrCreateTable(oldTable, &mask, relations)
+	startIdx := uintptr(newTable.Len())
+	count := uintptr(oldLen)
+	newTable.Alloc(uint32(count))
+
+	var i uintptr
+	for i = 0; i < count; i++ {
+		idx := startIdx + i
+		entity := oldTable.GetEntity(i)
+		index := &w.storage.entities[entity.id]
+		index.table = newTable.id
+		index.row = uint32(idx)
+	}
+
+	newTable.AddAllEntities(oldTable)
+	for _, id := range oldIDs {
+		if mask.Get(id) {
+			oldCol := oldTable.GetColumn(id)
+			newCol := newTable.GetColumn(id)
+			newCol.AddAll(oldCol)
+		}
+	}
+	if addComps != nil {
+		if len(add) != len(addComps) {
+			panic("lengths of IDs and components to add do not match")
+		}
+		for i := range count {
+			for j, id := range add {
+				newTable.Set(id, uint32(startIdx+i), addComps[j])
+			}
+		}
+	}
+
+	oldTable.Reset()
+	w.storage.registerTargets(relations)
+
+	return newTable, startIdx
+}
+
+func (w *World) getTables(batch *Batch) []*table {
+	tables := []*table{}
+
+	for i := range w.storage.archetypes {
+		archetype := &w.storage.archetypes[i]
+		if !batch.filter.matches(&archetype.mask) {
+			continue
+		}
+
+		if !archetype.HasRelations() {
+			table := &w.storage.tables[archetype.tables[0]]
+			tables = append(tables, table)
+			continue
+		}
+
+		tableIDs := archetype.GetTables(batch.relations)
+		for _, tab := range tableIDs {
+			table := &w.storage.tables[tab]
+			if !table.Matches(batch.relations) {
+				continue
+			}
+			tables = append(tables, table)
+		}
+	}
+	return tables
+}
+
 // setRelations sets the target entities for an entity relations.
 func (w *World) setRelations(entity Entity, relations []RelationID) {
 	w.checkLocked()
