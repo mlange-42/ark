5b2f6269dbaa2bee97f5c861fd50083333100627
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-03-03 01:17:29 +0100

    Set up user guide (#106)

---
+++ docs/content/quickstart/example_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"math/rand/v2"
+
+	"github.com/mlange-42/ark/ecs"
+)
+
+// Position component
+type Position struct {
+	X float64
+	Y float64
+}
+
+// Velocity component
+type Velocity struct {
+	X float64
+	Y float64
+}
+
+func main() {
+	// Create a World with given initial capacity.
+	world := ecs.NewWorld(1024)
+
+	// Create a component mapper.
+	mapper := ecs.NewMap2[Position, Velocity](&world)
+
+	// Create entities.
+	for range 1000 {
+		// Create a new Entity with components.
+		_ = mapper.NewEntity(
+			&Position{X: rand.Float64() * 100, Y: rand.Float64() * 100},
+			&Velocity{X: rand.NormFloat64(), Y: rand.NormFloat64()},
+		)
+	}
+
+	// Create a generic filter.
+	filter := ecs.NewFilter2[Position, Velocity](&world)
+
+	// Time loop.
+	for range 5000 {
+		// Get a fresh query.
+		query := filter.Query()
+		// Iterate it
+		for query.Next() {
+			// Component access through the Query.
+			pos, vel := query.Get()
+			// Update component fields.
+			pos.X += vel.X
+			pos.Y += vel.Y
+		}
+	}
+}
---
+++ docs/go.mod
@@ -0,0 +1,7 @@
+module github.com/mlange-42/ark/docs
+
+go 1.24
+
+require (
+	github.com/imfing/hextra v0.9.3 // indirect
+)
