ba6023bcb879c0acb36ca3303e670a8ff3b6daad
mlange-42 <martin_lange_@gmx.net>
2025-03-04 00:03:06 +0100

    fix for initial capacity

--- docs/content/world/world_test.go
+++ docs/content/world/world_test.go
@@ -7,12 +7,22 @@ import (
 )
 
 func TestWorldSimple(t *testing.T) {
+	world := ecs.NewWorld()
+	_ = world
+}
+
+func TestWorldConfig(t *testing.T) {
 	world := ecs.NewWorld(1024)
 	_ = world
 }
 
+func TestWorldConfigRelations(t *testing.T) {
+	world := ecs.NewWorld(1028, 128)
+	_ = world
+}
+
 func TestWorldReset(t *testing.T) {
-	world := ecs.NewWorld(1024)
+	world := ecs.NewWorld()
 	// ... do something with the world
 
 	world.Reset()
