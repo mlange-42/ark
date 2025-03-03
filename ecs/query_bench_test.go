f77ccb359b902c3f22e2f4acc6dd22c0a42f4b86
mlange-42 <martin_lange_@gmx.net>
2025-03-03 20:59:22 +0100

    Properly close cached queries

--- ecs/query_bench_test.go
+++ ecs/query_bench_test.go
@@ -20,6 +20,24 @@ func BenchmarkPosVelQuery_1000(b *testing.B) {
 	}
 }
 
+func BenchmarkPosVelQueryCached_1000(b *testing.B) {
+	n := 1000
+	world := NewWorld(128)
+
+	mapper := NewMap2[Position, Velocity](&world)
+	mapper.NewBatch(n, &Position{}, &Velocity{X: 1, Y: 0})
+
+	filter := NewFilter2[Position, Velocity](&world).Register()
+	for b.Loop() {
+		query := filter.Query()
+		for query.Next() {
+			pos, vel := query.Get()
+			pos.X += vel.X
+			pos.Y += vel.Y
+		}
+	}
+}
+
 func BenchmarkPosVelQueryUnsafe_1000(b *testing.B) {
 	n := 1000
 	world := NewWorld(128)
