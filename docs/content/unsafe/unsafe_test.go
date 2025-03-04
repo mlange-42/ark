e6bc2564093910fe6a8500ca3579cf06d7e9a88a
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-03-04 22:00:57 +0100

    User guide chapter on unsafe API (#127)

---
+++ docs/content/unsafe/unsafe_test.go
@@ -0,0 +1,61 @@
+package unsafe_test
+
+import (
+	"testing"
+
+	"github.com/mlange-42/ark/ecs"
+)
+
+var world = ecs.NewWorld()
+var posID = ecs.ComponentID[Position](&world)
+var velID = ecs.ComponentID[Velocity](&world)
+
+type Position struct {
+	X float64
+	Y float64
+}
+
+type Velocity struct {
+	X float64
+	Y float64
+}
+
+func TestUnsafeIDs(t *testing.T) {
+	world := ecs.NewWorld()
+
+	posID := ecs.ComponentID[Position](&world)
+	velID := ecs.ComponentID[Velocity](&world)
+
+	_, _ = posID, velID
+}
+
+func TestUnsafeNewEntity(t *testing.T) {
+	entity := world.Unsafe().NewEntity(posID, velID)
+	_ = entity
+}
+
+func TestUnsafeQuery(t *testing.T) {
+	filter := ecs.NewFilter(posID, velID)
+
+	query := filter.Query(&world)
+	for query.Next() {
+		pos := (*Position)(query.Get(posID))
+		vel := (*Velocity)(query.Get(velID))
+		pos.X += vel.X
+		pos.Y += vel.Y
+	}
+}
+
+func TestUnsafeGet(t *testing.T) {
+	entity := world.Unsafe().NewEntity(posID, velID)
+
+	pos := (*Position)(world.Unsafe().Get(entity, posID))
+	_ = pos
+}
+
+func TestUnsafeComponents(t *testing.T) {
+	entity := world.NewEntity()
+
+	world.Unsafe().Add(entity, posID, velID)
+	world.Unsafe().Remove(entity, posID)
+}
