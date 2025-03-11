b62a240caa4fce097bed5cb167603b430fcb8dc5
mlange-42 <martin_lange_@gmx.net>
2025-03-11 12:30:01 +0100

    tweak docstrings

--- ecs/map.go
+++ ecs/map.go
@@ -164,6 +164,9 @@ func (m *Map[T]) AddFn(entity Entity, fn func(a *T), target ...Entity) {
 }
 
 // Set the mapped component of the given entity to the given values.
+//
+// Requires the entity to already have the mapped component.
+// This is not a component operation, so it can be performed on a locked world.
 func (m *Map[T]) Set(entity Entity, comp *T) {
 	if !m.world.Alive(entity) {
 		panic("can't set component of a dead entity")
