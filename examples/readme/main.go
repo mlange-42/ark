2702007f768291cb757244eee24b18fea67d8289
mlange-42 <martin_lange_@gmx.net>
2025-03-24 23:09:15 +0100

    Tweak comments on quickstart/README example

--- examples/readme/main.go
+++ examples/readme/main.go
@@ -21,33 +21,35 @@ type Velocity struct {
 }
 
 func main() {
-	// Create a new World.
+	// Create a new World
 	world := ecs.NewWorld()
 
-	// Create a component mapper.
+	// Create a component mapper
+	// Save mappers permanently and re-use them for best performance
 	mapper := ecs.NewMap2[Position, Velocity](&world)
 
-	// Create entities.
+	// Create entities
 	for range 1000 {
-		// Create a new Entity with components.
+		// Create a new Entity with components
 		_ = mapper.NewEntity(
 			&Position{X: rand.Float64() * 100, Y: rand.Float64() * 100},
 			&Velocity{X: rand.NormFloat64(), Y: rand.NormFloat64()},
 		)
 	}
 
-	// Create a filter.
+	// Create a filter
+	// Save filters permanently and re-use them for best performance
 	filter := ecs.NewFilter2[Position, Velocity](&world)
 
-	// Time loop.
+	// Time loop
 	for range 5000 {
-		// Get a fresh query.
+		// Get a fresh query
 		query := filter.Query()
-		// Iterate it
+		// Iterate it.
 		for query.Next() {
-			// Component access through the Query.
+			// Component access through the Query
 			pos, vel := query.Get()
-			// Update component fields.
+			// Update component fields
 			pos.X += vel.X
 			pos.Y += vel.Y
 		}
