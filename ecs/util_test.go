9f312ab316bb4b9f5595ff55569fae7820b588d1
Luc Drenth <luc.drenth@hotmail.com>
2025-04-20 16:55:54 +0200

    add test for copyPtr

--- ecs/util_test.go
+++ ecs/util_test.go
@@ -1,7 +1,9 @@
 package ecs
 
 import (
+	"reflect"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,6 +11,48 @@ import (
 func TestTypeOf(t *testing.T) {
 	posType := typeOf[Position]()
 	assert.Equal(t, "Position", posType.Name())
+}
+
+func TestCopyPtr(t *testing.T) {
+	assert := assert.New(t)
+
+	type itemType uint8 // can be any type, result stays the same
+
+	// setup
+	var item itemType = 3
+	typeOfItem := reflect.TypeOf(item)
+	itemSize := sizeOf(typeOfItem)
+	targetItemIndex := 6
+	totalItems := 10
+	data := reflect.New(reflect.ArrayOf(int(totalItems), typeOfItem)).Elem()
+	dataPointer := data.Addr().UnsafePointer() // points to the start of data
+
+	getItem := func(index int) *itemType {
+		return (*itemType)(unsafe.Add(dataPointer, uintptr(index)*itemSize))
+	}
+
+	// check that the expected item is not there yet
+	for i := range totalItems {
+		assert.Equal(itemType(0), *getItem(i))
+	}
+
+	// copy the item to the right place
+	destination := unsafe.Add(
+		dataPointer,
+		uintptr(targetItemIndex)*itemSize,
+	)
+
+	source := unsafe.Pointer(&item)
+	copyPtr(source, destination, itemSize)
+
+	// check that only the expected item is now set
+	for i := range totalItems {
+		if i == targetItemIndex {
+			assert.Equal(item, *getItem(i))
+		} else {
+			assert.Equal(itemType(0), *getItem(i))
+		}
+	}
 }
 
 func TestPagedSlice(t *testing.T) {
