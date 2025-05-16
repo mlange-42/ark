7144155c6587e4a72c6dcacdc2b65699414d6a63
mlange-42 <martin_lange_@gmx.net>
2025-05-16 13:51:03 +0200

    Add a test for using interfaces as resources

--- ecs/resource_test.go
+++ ecs/resource_test.go
@@ -24,6 +24,32 @@ func TestResource(t *testing.T) {
 	assert.False(t, get.Has())
 }
 
+type ResInterface interface {
+	MyMethod() string
+}
+
+type ResImpl struct{}
+
+func NewRes() ResInterface {
+	return &ResImpl{}
+}
+
+func (r *ResImpl) MyMethod() string {
+	return "test"
+}
+
+func TestResourceInterface(t *testing.T) {
+	w := ecs.NewWorld()
+
+	res := NewRes()
+	ecs.AddResource(&w, &res)
+
+	resOut := *ecs.GetResource[ResInterface](&w)
+
+	assert.NotNil(t, resOut)
+	assert.Equal(t, "test", resOut.MyMethod())
+}
+
 func ExampleResource() {
 	// Create a world.
 	world := ecs.NewWorld()
