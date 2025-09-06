2e5f34721f2bf161c002c054e236021e60bbf93b
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-09-06 13:59:38 +0200

    Fix component to archetype mapping for pointer persistence (#277)

--- benchmark/table/main.go
+++ benchmark/table/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/mlange-42/ark/benchmark"
 )
 
-const version = "v0.4.7"
+const version = "v0.4.8"
 
 func main() {
 	fmt.Printf("Last run: %s  \n", time.Now().Format(time.RFC1123))
