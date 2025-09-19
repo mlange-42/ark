b8d57008c8fc3fce4df53bdfc4db2f45a80ba643
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-09-19 13:32:23 +0200

    Add benchmarks for `World.Stats` (#310)

--- ecs/world_bench_test.go
+++ ecs/world_bench_test.go
@@ -1,16 +1,14 @@
-package ecs_test
+package ecs
 
 import (
+	"math"
 	"testing"
-
-	"github.com/mlange-42/ark/ecs"
 )
 
 func BenchmarkCreateEntity1Comp_1000(b *testing.B) {
-
-	w := ecs.NewWorld()
-	builder := ecs.NewMap1[Position](&w)
-	filter := ecs.NewFilter0(&w)
+	w := NewWorld()
+	builder := NewMap1[Position](&w)
+	filter := NewFilter0(&w)
 
 	builder.NewBatchFn(1000, nil)
 	w.RemoveEntities(filter.Batch(), nil)
@@ -23,4 +21,122 @@ func BenchmarkCreateEntity1Comp_1000(b *testing.B) {
 		w.RemoveEntities(filter.Batch(), nil)
 		b.StartTimer()
 	}
+}
+
+func BenchmarkWorldStats4Arch(b *testing.B) {
+	w := NewWorld()
+
+	idA := ComponentID[CompA](&w)
+	idB := ComponentID[CompB](&w)
+
+	u := w.Unsafe()
+	u.NewEntity()
+	u.NewEntity(idA)
+	u.NewEntity(idB)
+	u.NewEntity(idA, idB)
+
+	stats := w.Stats()
+
+	for b.Loop() {
+		stats = w.Stats()
+	}
+	expectEqual(b, 4, len(stats.Archetypes))
+}
+
+func BenchmarkWorldStats16Arch(b *testing.B) {
+	w := NewWorld()
+
+	allIDs := []ID{
+		ComponentID[CompA](&w),
+		ComponentID[CompB](&w),
+		ComponentID[CompC](&w),
+		ComponentID[CompD](&w),
+	}
+
+	ids := []ID{}
+	for i := range int(math.Pow(2, float64(len(allIDs)))) {
+		for j, id := range allIDs {
+			m := 1 << j
+			if i&m == m {
+				ids = append(ids, id)
+			}
+		}
+		w.Unsafe().NewEntity(ids...)
+		ids = ids[:0]
+	}
+
+	stats := w.Stats()
+
+	for b.Loop() {
+		stats = w.Stats()
+	}
+	expectEqual(b, 16, len(stats.Archetypes))
+}
+
+func BenchmarkWorldStats64Arch(b *testing.B) {
+	w := NewWorld()
+
+	allIDs := []ID{
+		ComponentID[CompA](&w),
+		ComponentID[CompB](&w),
+		ComponentID[CompC](&w),
+		ComponentID[CompD](&w),
+		ComponentID[CompE](&w),
+		ComponentID[CompF](&w),
+	}
+
+	ids := []ID{}
+	for i := range int(math.Pow(2, float64(len(allIDs)))) {
+		for j, id := range allIDs {
+			m := 1 << j
+			if i&m == m {
+				ids = append(ids, id)
+			}
+		}
+		w.Unsafe().NewEntity(ids...)
+		ids = ids[:0]
+	}
+
+	stats := w.Stats()
+
+	for b.Loop() {
+		stats = w.Stats()
+	}
+	expectEqual(b, 64, len(stats.Archetypes))
+}
+
+func BenchmarkWorldStats1024Arch(b *testing.B) {
+	w := NewWorld()
+
+	allIDs := []ID{
+		ComponentID[CompA](&w),
+		ComponentID[CompB](&w),
+		ComponentID[CompC](&w),
+		ComponentID[CompD](&w),
+		ComponentID[CompE](&w),
+		ComponentID[CompF](&w),
+		ComponentID[CompG](&w),
+		ComponentID[CompH](&w),
+		ComponentID[CompI](&w),
+		ComponentID[CompJ](&w),
+	}
+
+	ids := []ID{}
+	for i := range int(math.Pow(2, float64(len(allIDs)))) {
+		for j, id := range allIDs {
+			m := 1 << j
+			if i&m == m {
+				ids = append(ids, id)
+			}
+		}
+		w.Unsafe().NewEntity(ids...)
+		ids = ids[:0]
+	}
+
+	stats := w.Stats()
+
+	for b.Loop() {
+		stats = w.Stats()
+	}
+	expectEqual(b, 1024, len(stats.Archetypes))
 }
