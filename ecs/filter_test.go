b21ffd57c825248630ce0965d1b82431141bf0f9
mlange-42 <martin_lange_@gmx.net>
2025-09-21 15:56:16 +0200

    add filter benchmarks

--- ecs/filter_test.go
+++ ecs/filter_test.go
@@ -40,3 +40,30 @@ func BenchmarkFilterCopy(b *testing.B) {
 	}
 	_ = ff
 }
+
+func BenchmarkFilterMatches(b *testing.B) {
+	filterMask := newMask256(id(1), id(2))
+	filter := filter{
+		mask: filterMask,
+	}
+	mask := newMask256(id(1), id(2), id(3))
+
+	for b.Loop() {
+		_ = filter.matches(&mask)
+	}
+}
+
+func BenchmarkFilterWithoutMatches(b *testing.B) {
+	filterMask := newMask256(id(1), id(2))
+	withoutMask := newMask256(id(1), id(2))
+	filter := filter{
+		mask:       filterMask,
+		without:    withoutMask,
+		hasWithout: true,
+	}
+	mask := newMask256(id(1), id(2), id(3))
+
+	for b.Loop() {
+		_ = filter.matches(&mask)
+	}
+}
