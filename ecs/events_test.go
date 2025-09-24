fa6ac4c07c052de1d9ef75ca8f4efa2ef7cac2ed
mlange-42 <martin_lange_@gmx.net>
2025-09-24 23:32:15 +0200

    test OnCreateEntity

--- ecs/events_test.go
+++ ecs/events_test.go
@@ -55,3 +55,36 @@ func TestObserverManager(t *testing.T) {
 			m.RemoveObserver(&Observer{id: 13})
 		})
 }
+
+func TestObserverOnCreateEntity(t *testing.T) {
+	w := NewWorld()
+
+	posID := ComponentID[Position](&w)
+	velID := ComponentID[Velocity](&w)
+	builder1 := NewMap1[Position](&w)
+	builder2 := NewMap1[Velocity](&w)
+
+	callCount := 0
+
+	NewObserver(OnCreateEntity,
+		func(e Entity) {
+			callCount++
+		}).
+		With(C[Position]()).
+		Register(&w)
+
+	builder1.NewEntity(&Position{})
+	expectEqual(t, 1, callCount)
+	builder2.NewEntity(&Velocity{})
+	expectEqual(t, 1, callCount)
+
+	w.Unsafe().NewEntity(posID)
+	expectEqual(t, 2, callCount)
+	w.Unsafe().NewEntity(velID)
+	expectEqual(t, 2, callCount)
+
+	builder1.NewBatch(10, &Position{})
+	expectEqual(t, 12, callCount)
+	builder2.NewBatch(10, &Velocity{})
+	expectEqual(t, 12, callCount)
+}
