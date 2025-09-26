c356537a9dd15ae57229e957fe3d186f9f9360ab
mlange-42 <martin_lange_@gmx.net>
2025-09-26 19:12:22 +0200

    prototype generic observer

---
+++ ecs/observers_test.go
@@ -0,0 +1,21 @@
+package ecs
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestObserver2(t *testing.T) {
+	w := NewWorld()
+	builder := NewMap2[Position, Velocity](&w)
+
+	Observe2[Position, Velocity](OnAddComponents).
+		Do(func(e Entity, p *Position, v *Velocity) {
+			fmt.Printf("%#v\n", p)
+			fmt.Printf("%#v\n", v)
+		}).
+		Register(&w)
+
+	e := w.NewEntity()
+	builder.Add(e, &Position{}, &Velocity{})
+}
