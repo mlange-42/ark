339c271730b23dc785ca2b0be0366317bcd02b3e
mlange-42 <martin_lange_@gmx.net>
2025-09-29 11:26:52 +0200

    Add examples for built-in and custom events

--- examples/custom_events/main.go
+++ examples/custom_events/main.go
@@ -1,1 +1,38 @@
+// Demonstrates how to use the custom event types.
 package main
+
+import (
+	"fmt"
+
+	"github.com/mlange-42/ark/ecs"
+)
+
+// Position component
+type Position struct {
+	X int
+	Y int
+}
+
+// OnTeleport is a custom event type.
+var OnTeleport = ecs.NewEventType()
+
+func main() {
+	// Create a new World
+	world := ecs.NewWorld()
+
+	// Register an observer for the custom event, observing Position
+	ecs.Observe1[Position](OnTeleport).
+		Do(func(e ecs.Entity, pos *Position) {
+			fmt.Printf("Entity teleported to: %#v\n", *pos)
+		}).
+		Register(&world)
+
+	// Create an entity with Position
+	builder := ecs.NewMap1[Position](&world)
+	entity := builder.NewEntity(&Position{X: 0, Y: 0})
+
+	// Create a custom event for teleports
+	event := world.Event(OnTeleport).For(ecs.C[Position]())
+	// Emit the event for the entity
+	event.Emit(entity)
+}
--- examples/events/main.go
+++ examples/events/main.go
@@ -1,1 +1,52 @@
+// Demonstrates how to use the built-in event types.
 package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+
+	"github.com/mlange-42/ark/ecs"
+)
+
+// Position component
+type Position struct {
+	X int
+	Y int
+}
+
+// Velocity component
+type Velocity struct {
+	X int
+	Y int
+}
+
+func main() {
+	// Create a new World
+	world := ecs.NewWorld()
+
+	// Register an observer for entity creation, observing Position and Velocity
+	ecs.Observe2[Position, Velocity](ecs.OnCreateEntity).
+		Do(func(e ecs.Entity, pos *Position, vel *Velocity) {
+			fmt.Printf("Create entity with %#v, %#v\n", *pos, *vel)
+		}).
+		Register(&world)
+
+	// Register an observer for entity removal, observing Position and Velocity
+	ecs.Observe2[Position, Velocity](ecs.OnRemoveEntity).
+		Do(func(e ecs.Entity, pos *Position, vel *Velocity) {
+			fmt.Printf("Remove entity with %#v, %#v\n", *pos, *vel)
+		}).
+		Register(&world)
+
+	// A mapper for creating entities with Position and Velocity
+	builder := ecs.NewMap2[Position, Velocity](&world)
+
+	// Create an entity with Position and Velocity
+	entity := builder.NewEntity(
+		&Position{X: rand.IntN(100), Y: rand.IntN(100)},
+		&Velocity{},
+	)
+
+	// Remove the entity
+	world.RemoveEntity(entity)
+}
