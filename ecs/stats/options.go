81c7e91f2734f99155e51a8bf3a27defe1cbe719
mlange-42 <martin_lange_@gmx.net>
2025-10-06 14:27:36 +0200

    remove filters and observers option

--- ecs/stats/options.go
+++ ecs/stats/options.go
@@ -8,15 +8,11 @@ const (
 	Archetypes Option = 1 << iota
 	// Tables included in stats. Requires Archetypes.
 	Tables
-	// Filters included in stats.
-	Filters
-	// Observers included in stats.
-	Observers
 )
 
 const (
 	// None discards all stats details.
 	None Option = iota
 	// All includes all stats details.
-	All Option = Archetypes | Tables | Filters | Observers
+	All Option = Archetypes | Tables
 )
