80836f40609a90910deed6c42bca7cce3d1c2c66
mlange-42 <martin_lange_@gmx.net>
2025-10-07 21:30:48 +0200

    add/remove entities

--- docs/content/cheatsheet/cheatsheet_test.go
+++ docs/content/cheatsheet/cheatsheet_test.go
@@ -7,8 +7,6 @@ import (
 	"github.com/mlange-42/ark/ecs"
 )
 
-var world = ecs.NewWorld()
-
 type Position struct {
 	X float64
 	Y float64
@@ -23,27 +21,64 @@ type Altitude struct {
 	Z float64
 }
 
-func TestCreateEntities(t *testing.T) {
-	// Create a component mapper
-	mapper := ecs.NewMap2[Position, Velocity](&world)
+var world = ecs.NewWorld()
+var mapper = ecs.NewMap2[Position, Velocity](&world)
+var filter = ecs.NewFilter2[Position, Velocity](&world).Exclusive()
+var entity = world.NewEntity()
 
-	// Create an entity, giving components
-	mapper.NewEntity(&Position{}, &Velocity{X: 1, Y: -1})
-	// Create an entity, using a callback
-	mapper.NewEntityFn(func(pos *Position, vel *Velocity) {
-		pos.X, pos.Y = 0, 0
+func TestCreateEmpty(t *testing.T) {
+	e := world.NewEntity()
+	_ = e
+}
+
+func TestCreateMapper(t *testing.T) {
+	mapper := ecs.NewMap2[Position, Velocity](&world)
+	_ = mapper
+}
+
+func TestCreateEntity(t *testing.T) {
+	e := mapper.NewEntity(
+		&Position{X: 100, Y: 100},
+		&Velocity{X: 1, Y: -1},
+	)
+	_ = e
+}
+
+func TestCreateEntityFn(t *testing.T) {
+	e := mapper.NewEntityFn(func(pos *Position, vel *Velocity) {
+		pos.X, pos.Y = 100, 100
 		vel.X, vel.Y = 1, -1
 	})
+	_ = e
+}
 
-	// Create many entities, all with the same component values
-	mapper.NewBatch(100, &Position{}, &Velocity{X: 1, Y: -1})
-	// Create many entities, using a callback (set values individually)
+func TestCreateBatch(t *testing.T) {
+	mapper.NewBatch(
+		100,
+		&Position{X: 100, Y: 100},
+		&Velocity{X: 1, Y: -1},
+	)
+}
+
+func TestCreateBatchFn(t *testing.T) {
 	mapper.NewBatchFn(100, func(e ecs.Entity, pos *Position, vel *Velocity) {
 		pos.X, pos.Y = rand.Float64()*100, rand.Float64()*100
 		vel.X, vel.Y = rand.NormFloat64(), rand.NormFloat64()
 	})
 }
 
-func TestRemoveEntities(t *testing.T) {
+func TestRemoveEntity(t *testing.T) {
+	world.RemoveEntity(entity)
+}
 
+func TestRemoveEntities(t *testing.T) {
+	filter := ecs.NewFilter2[Position, Velocity](&world).Exclusive()
+
+	world.RemoveEntities(filter.Batch(), nil)
+}
+
+func TestRemoveEntitiesFn(t *testing.T) {
+	world.RemoveEntities(filter.Batch(), func(entity ecs.Entity) {
+		// Do something before removal
+	})
 }
--- docs/content/cheatsheet/index.md
+++ docs/content/cheatsheet/index.md
@@ -6,4 +6,42 @@ description = "Cheat sheet for frequent use cases."
 +++
 ## Create entities
 
-{{< code-func cheatsheet_test.go TestCreateEntities >}}
+Create an **entity without components**:
+
+{{< code-func cheatsheet_test.go TestCreateEmpty >}}
+
+For creating **entities with components**, you need a **component mapper**:
+
+{{< code-func cheatsheet_test.go TestCreateMapper >}}
+
+Create a **single entity**, given some components:
+
+{{< code-func cheatsheet_test.go TestCreateEntity >}}
+
+Create a **single entity** using a callback:
+
+{{< code-func cheatsheet_test.go TestCreateEntityFn >}}
+
+Create **many entities** more efficiently, all with the same component values:
+
+{{< code-func cheatsheet_test.go TestCreateBatch >}}
+
+Create **many entities**, using a callback for individual initialization:
+
+{{< code-func cheatsheet_test.go TestCreateBatchFn >}}
+
+## Remove entities
+
+Remove a **single entity**:
+
+{{< code-func cheatsheet_test.go TestRemoveEntity >}}
+
+Remove **all entities** that match a filter:
+
+{{< code-func cheatsheet_test.go TestRemoveEntities >}}
+
+You can use a callback to do something with entities before their removal:
+
+{{< code-func cheatsheet_test.go TestRemoveEntitiesFn >}}
+
+## Add/remove components
