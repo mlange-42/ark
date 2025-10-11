c393235965be9432dc463787e7c75fccd51505e4
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-10-11 13:49:59 +0200

    Run table benchmarks 3 times (#406)

--- benchmark/table/main.go
+++ benchmark/table/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/klauspost/cpuid/v2"
@@ -12,18 +14,23 @@ const version = "v0.5.3-dev"
 const goVersion = "1.25.1"
 
 func main() {
+	repetitions, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Printf("Last run: %s  \n", time.Now().Format(time.RFC1123))
 	fmt.Printf("Version: Ark %s  \n", version)
 	fmt.Printf("Go version: %s  \n", goVersion)
 	fmt.Printf("CPU: %s\n\n", cpuid.CPU.BrandName)
 
-	benchmark.RunBenchmarks("Query", benchesQuery(), benchmark.ToMarkdown)
-	benchmark.RunBenchmarks("World access", benchesWorld(), benchmark.ToMarkdown)
-	benchmark.RunBenchmarks("Entities", benchesEntities(), benchmark.ToMarkdown)
-	benchmark.RunBenchmarks("Entities, batched", benchesEntitiesBatch(), benchmark.ToMarkdown)
-	benchmark.RunBenchmarks("Components", benchesComponents(), benchmark.ToMarkdown)
-	benchmark.RunBenchmarks("Components, batched", benchesComponentsBatch(), benchmark.ToMarkdown)
-	benchmark.RunBenchmarks("Other", benchesOther(), benchmark.ToMarkdown)
+	benchmark.RunBenchmarks("Query", benchesQuery(), repetitions, benchmark.ToMarkdown)
+	benchmark.RunBenchmarks("World access", benchesWorld(), repetitions, benchmark.ToMarkdown)
+	benchmark.RunBenchmarks("Entities", benchesEntities(), repetitions, benchmark.ToMarkdown)
+	benchmark.RunBenchmarks("Entities, batched", benchesEntitiesBatch(), repetitions, benchmark.ToMarkdown)
+	benchmark.RunBenchmarks("Components", benchesComponents(), repetitions, benchmark.ToMarkdown)
+	benchmark.RunBenchmarks("Components, batched", benchesComponentsBatch(), repetitions, benchmark.ToMarkdown)
+	benchmark.RunBenchmarks("Other", benchesOther(), repetitions, benchmark.ToMarkdown)
 
 	fmt.Print("\n\n")
 }
