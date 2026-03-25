f50b55c1fea4388f19c2d5aac5e344027f42dd74
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2026-03-25 14:42:40 +0100

    Add entity creation benchmarks to regression tests (#477)

---
+++ benchmark/compare/new_entity.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/mlange-42/ark/ecs"
+)
+
+func newEntity2Comp10(b *testing.B) {
+	newEntity2Comp(b, 10)
+}
+
+func newEntity2Comp1000(b *testing.B) {
+	newEntity2Comp(b, 1000)
+}
+
+func newEntity2Comp(b *testing.B, n int) {
+	world := ecs.NewWorld()
+
+	mapper := ecs.NewMap2[Position, Velocity](world)
+	mapper.NewBatchFn(n, nil)
+
+	filter := ecs.NewFilter2[Position, Velocity](world)
+	world.RemoveEntities(filter.Batch(), nil)
+
+	entities := make([]ecs.Entity, 0, n)
+
+	for b.Loop() {
+		for range n {
+			e := mapper.NewEntityFn(nil)
+			// Just for fairness, because the others need to do that, too.
+			entities = append(entities, e)
+		}
+		b.StopTimer()
+
+		if n < 64 {
+			// Speed up cleanup for low entity counts
+			for i := len(entities) - 1; i >= 0; i-- {
+				world.RemoveEntity(entities[i])
+			}
+		} else {
+			world.RemoveEntities(filter.Batch(), nil)
+		}
+
+		entities = entities[:0]
+		b.StartTimer()
+	}
+}
+
+func newEntity5Comp10(b *testing.B) {
+	newEntity5Comp(b, 10)
+}
+
+func newEntity5Comp1000(b *testing.B) {
+	newEntity5Comp(b, 1000)
+}
+
+func newEntity5Comp(b *testing.B, n int) {
+	world := ecs.NewWorld()
+
+	mapper := ecs.NewMap5[Comp1, Comp2, Comp3, Comp4, Comp5](world)
+	mapper.NewBatchFn(n, nil)
+
+	filter := ecs.NewFilter5[Comp1, Comp2, Comp3, Comp4, Comp5](world)
+	world.RemoveEntities(filter.Batch(), nil)
+
+	entities := make([]ecs.Entity, 0, n)
+
+	for b.Loop() {
+		for range n {
+			e := mapper.NewEntityFn(nil)
+			// Just for fairness, because the others need to do that, too.
+			entities = append(entities, e)
+		}
+		b.StopTimer()
+
+		if n < 64 {
+			// Speed up cleanup for low entity counts
+			for i := len(entities) - 1; i >= 0; i-- {
+				world.RemoveEntity(entities[i])
+			}
+		} else {
+			world.RemoveEntities(filter.Batch(), nil)
+		}
+
+		entities = entities[:0]
+		b.StartTimer()
+	}
+}
--- benchmark/compare/types.go
+++ benchmark/compare/types.go
@@ -11,3 +11,63 @@ type Velocity struct {
 	X float64
 	Y float64
 }
+
+// Comp1 component.
+type Comp1 struct {
+	X float64
+	Y float64
+}
+
+// Comp2 component.
+type Comp2 struct {
+	X float64
+	Y float64
+}
+
+// Comp3 component.
+type Comp3 struct {
+	X float64
+	Y float64
+}
+
+// Comp4 component.
+type Comp4 struct {
+	X float64
+	Y float64
+}
+
+// Comp5 component.
+type Comp5 struct {
+	X float64
+	Y float64
+}
+
+// Comp6 component.
+type Comp6 struct {
+	X float64
+	Y float64
+}
+
+// Comp7 component.
+type Comp7 struct {
+	X float64
+	Y float64
+}
+
+// Comp8 component.
+type Comp8 struct {
+	X float64
+	Y float64
+}
+
+// Comp9 component.
+type Comp9 struct {
+	X float64
+	Y float64
+}
+
+// Comp10 component.
+type Comp10 struct {
+	X float64
+	Y float64
+}
