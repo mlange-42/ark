ea2f021ac0098c7243de88174c535fa7737416d3
mlange-42 <martin_lange_@gmx.net>
2026-03-25 10:19:49 +0100

    implement CSV writing

--- benchmark/util.go
+++ benchmark/util.go
@@ -2,6 +2,7 @@ package benchmark
 
 import (
 	"fmt"
+	"io"
 	"strings"
 	"testing"
 )
@@ -13,29 +14,43 @@ type Benchmark struct {
 	F      func(b *testing.B)
 	N      int
 	T      float64
+	Mem    float64
 	Factor float64
 	Units  string
 }
 
+// Format for writing benchmark results.
+type Format struct {
+	Format func(string, []Benchmark) string
+	Writer io.Writer
+}
+
 // RunBenchmarks runs the benchmarks and prints the results.
-func RunBenchmarks(title string, benches []Benchmark, count int, format func([]Benchmark) string) {
+func RunBenchmarks(title string, benches []Benchmark, count int, format []Format) {
 	for i := range benches {
 		b := &benches[i]
 		var t int64
 		var n int
+		var mem int
 		for range count {
 			res := testing.Benchmark(b.F)
 			t += res.T.Nanoseconds()
 			n += res.N
+			mem += int(res.MemBytes)
 		}
 		b.T = float64(t) / float64(n*b.N)
+		b.Mem = float64(mem) / float64(n*b.N)
 	}
-	fmt.Printf("## %s\n\n%s", title, format(benches))
+	for _, f := range format {
+		fmt.Fprint(f.Writer, f.Format(title, benches))
+	}
 }
 
 // ToMarkdown converts the benchmarks to a markdown table.
-func ToMarkdown(benches []Benchmark) string {
+func ToMarkdown(title string, benches []Benchmark) string {
 	b := strings.Builder{}
+
+	b.WriteString(fmt.Sprintf("## %s\n\n", title))
 
 	b.WriteString(fmt.Sprintf("| %-38s | %-12s | %-28s |\n", "Operation", "Time", "Remark"))
 	b.WriteString(fmt.Sprintf("|%s|%s:|%s|\n", strings.Repeat("-", 40), strings.Repeat("-", 13), strings.Repeat("-", 30)))
@@ -53,6 +68,21 @@ func ToMarkdown(benches []Benchmark) string {
 
 		t := fmt.Sprintf("%.1f %s", bench.T*factor, units)
 		b.WriteString(fmt.Sprintf("| %-38s | %12s | %-28s |\n", bench.Name, t, bench.Desc))
+	}
+	b.WriteString("\n")
+
+	return b.String()
+}
+
+// ToCSV converts the benchmarks to a CSV table.
+func ToCSV(title string, benches []Benchmark) string {
+	b := strings.Builder{}
+
+	b.WriteString(fmt.Sprintf("%s;%s;%s\n", "Operation", "Time", "Alloc"))
+
+	for i := range benches {
+		bench := &benches[i]
+		b.WriteString(fmt.Sprintf("%s;%0.2f;%0.2f\n", bench.Name, bench.T, bench.Mem))
 	}
 	b.WriteString("\n")
 
