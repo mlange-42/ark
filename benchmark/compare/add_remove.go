cfbb84c49c2e3854f77ac4d35b1ebb925337e7cd
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2026-03-31 22:25:54 +0200

    Run batch add/remove benchmark with more entities (#495)

--- benchmark/compare/add_remove.go
+++ benchmark/compare/add_remove.go
@@ -90,10 +90,46 @@ func addRemoveBatch1000(b *testing.B) {
 	addRemoveBatch(b, 1000)
 }
 
+func addRemoveBatch100000(b *testing.B) {
+	addRemoveBatch(b, 100000)
+}
+
 func addRemoveBatch(b *testing.B, n int) {
 	world := ecs.NewWorld()
 
 	posMap := ecs.NewMap1[Position](world)
+	velMap := ecs.NewMap1[Velocity](world)
+
+	posFilter := ecs.NewFilter1[Position](world)
+	velFilter := ecs.NewFilter1[Velocity](world)
+
+	posMap.NewBatchFn(n, nil)
+
+	velMap.AddBatchFn(posFilter.Batch(), nil)
+	velMap.RemoveBatch(velFilter.Batch(), nil)
+
+	for b.Loop() {
+		velMap.AddBatchFn(posFilter.Batch(), nil)
+		velMap.RemoveBatch(velFilter.Batch(), nil)
+	}
+}
+
+func addRemoveBatchLarge10(b *testing.B) {
+	addRemoveBatchLarge(b, 10)
+}
+
+func addRemoveBatchLarge1000(b *testing.B) {
+	addRemoveBatchLarge(b, 1000)
+}
+
+func addRemoveBatchLarge100000(b *testing.B) {
+	addRemoveBatchLarge(b, 100000)
+}
+
+func addRemoveBatchLarge(b *testing.B, n int) {
+	world := ecs.NewWorld()
+
+	posMap := ecs.NewMap11[Position, Comp1, Comp2, Comp3, Comp4, Comp5, Comp6, Comp7, Comp8, Comp9, Comp10](world)
 	velMap := ecs.NewMap1[Velocity](world)
 
 	posFilter := ecs.NewFilter1[Position](world)
--- benchmark/compare/main.go
+++ benchmark/compare/main.go
@@ -76,5 +76,10 @@ func benchesCompare() []benchmark.Benchmark {
 
 		{Name: "Add/Remove batch 1 of 2", Desc: "", F: addRemoveBatch10, N: 10},
 		{Name: "Add/Remove batch 1 of 2", Desc: "", F: addRemoveBatch1000, N: 1000},
+		{Name: "Add/Remove batch 1 of 2", Desc: "", F: addRemoveBatch100000, N: 100000},
+
+		{Name: "Add/Remove batch 1 of 11", Desc: "", F: addRemoveBatchLarge10, N: 10},
+		{Name: "Add/Remove batch 1 of 11", Desc: "", F: addRemoveBatchLarge1000, N: 1000},
+		{Name: "Add/Remove batch 1 of 11", Desc: "", F: addRemoveBatchLarge100000, N: 100000},
 	}
 }
